def61989800e399b149b2781119f6651f6cf043f
SuperMardle <matao.mardle@gmail.com>
2016-05-26 22:31:40 +0800

    [ADD] Add gcd

---
+++ src/Basics/GCD.java
@@ -0,0 +1,18 @@
+package Basics;
+
+/**
+ * Created by matao on 5/26/16.
+ */
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcd(20, 5));
+    }
+
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        int r = p % q;
+        return gcd(q, r);
+    }
+}
