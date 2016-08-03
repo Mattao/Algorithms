5da3a197848aa53a2ab77bcf795d8ac6f0b8e5cb
SuperMardle <matao.mardle@gmail.com>
2016-08-03 23:31:54 +0800

    [REV]

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import edu.princeton.cs.algs4.StdDraw;
+import java.security.InvalidParameterException;
 
 /**
  * Created by matao on 7/20/16.
@@ -6,14 +6,26 @@ import edu.princeton.cs.algs4.StdDraw;
 public class Test {
 
     public static void main(String[] args) {
-        int n = 100;
-        StdDraw.setXscale(0, n);
-        StdDraw.setYscale(0, n);
-        StdDraw.setPenRadius(.01);
-        for (int i = 1; i <= n; i++) {
-            StdDraw.point(i, i);
-//            StdDraw.point(i, i * i);
-//            StdDraw.point(i, i * Math.log(i));
+        System.out.println(recursionGcd(-1, 5));
+    }
+
+    /**
+     * 计算两个非负整数p和q的最大公约数: 若q是0, 则最大公约数为p
+     * 否则将p除以q得到余数r, p和q的最大公约数即q和r的最大公约数
+     *
+     * @param p     入参p
+     * @param q     入参q
+     * @return      最大公约数
+     */
+    public static int recursionGcd(int p, int q) {
+        if (p < 0 || q < 0) {
+            throw new InvalidParameterException("参数不能为负数");
         }
+
+        if (q == 0) {
+            return p;
+        }
+        int r = p % q;
+        return recursionGcd(q, r);
     }
 }
