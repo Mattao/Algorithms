92836dcf4155e574a24f88eb174ab4ec7e986dea
SuperMardle <matao.mardle@gmail.com>
2016-08-14 13:39:24 +0800

    [REV] Revise

--- src/Fundamentals/UnionFind/QuickUnion.java
+++ src/Fundamentals/UnionFind/QuickUnion.java
@@ -4,15 +4,13 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Created by matao on 8/13/16.
- * 保证当且仅当id[p]==id[q]时p和q是联通的。也就是说同一个连通分量中所有触点在id[]中的值都是相同的。
- * 这意味着connected(p, q)只需判断id[p] == id[q]
+ * Created by matao on 8/14/16.
  */
-public class QuickFind {
+public class QuickUnion {
     private int[] id;   // 分量id(以触电作为索引)
-    private int N;  // 分量数量
+    private int N;      // 分量数量
 
-    public QuickFind(int count) {
+    public QuickUnion(int count) {
         N = count;
         id = new int[N];
         // 初始化count个触点
@@ -36,8 +34,7 @@ public class QuickFind {
      * @return p和q是否存在于同一个连通分量中
      */
     public boolean connected(int p, int q) {
-        return id[p] == id[q];
-//        return find(p) == find(q);
+        return find(p) == find(q);
     }
 
     /**
@@ -45,7 +42,10 @@ public class QuickFind {
      * @return p所在分量的标示符(0 - N)
      */
     public int find(int p) {
-        return id[p];
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
     }
 
     /**
@@ -57,16 +57,11 @@ public class QuickFind {
      * @param q
      */
     public void union(int p, int q) {
-        int pId = find(p);
-        int qId = find(q);
-        if (pId == qId) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot)
             return;
-        }
-        for (int i = 0; i < id.length; i++) {
-            if (id[i] == pId) {
-                id[i] = qId;
-            }
-        }
+        id[pRoot] = qRoot;
         N--;
     }
 
@@ -74,15 +69,14 @@ public class QuickFind {
         // e.g. (4, 3)(3, 8)(6, 5)(9, 4)(2, 1)(8, 9)(5,0)(7,2)(6,1)(1,0)(6,7)
         // (8, 9)(1, 0)(6, 7)这三对将不会被输出
         int n = 10;
-        QuickFind uf = new QuickFind(n);
+        QuickUnion uf = new QuickUnion(n);
         while (!StdIn.isEmpty()) {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
-            if (uf.connected(p, q)) {
-                continue;
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
             }
-            uf.union(p, q);
-            StdOut.println(p + " " + q);
         }
         StdOut.println("count: " + uf.count());
     }
