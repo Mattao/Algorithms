1b90e32d5b777fcc4edb852c8a1b3524ac0859ef
matao <muxter.matao@gmail.com>
2017-11-29 23:01:36 +0800

    chore: rename

--- src/main/java/com/matao/fundamental/Gcd.java
+++ src/main/java/com/matao/fundamental/Gcd.java
@@ -6,18 +6,14 @@ package com.matao.fundamental;
  * 计算两个非负整数p和q的最大公约数: 若q是0, 则最大公约数为p
  * 否则将p除以q得到余数r, p和q的最大公约数即q和r的最大公约数
  */
-public class GCD {
-    public static void main(String[] args) {
-        System.out.println(gcd(20, 5));
-        System.out.println(recursionGcd(20, 5));
-    }
+public class Gcd {
 
-    public static int recursionGcd(int p, int q) {
+    public static int gcdByRecursion(int p, int q) {
         if (q == 0) {
             return p;
         }
         int r = p % q;
-        return recursionGcd(q, r);
+        return gcdByRecursion(q, r);
     }
 
     public static int gcd(int p, int q) {
@@ -27,5 +23,10 @@ public class GCD {
             q = r;
         }
         return p;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(20, 5));
+        System.out.println(gcdByRecursion(20, 5));
     }
 }
