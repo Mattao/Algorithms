dfe7c8c7d82e5156440eed0ac6b350c93c2022aa
matao <muxter.matao@gmail.com>
2017-11-29 23:37:50 +0800

    feat: palindrome

---
+++ src/main/java/com/matao/fundamental/Palindrome.java
@@ -0,0 +1,23 @@
+package com.matao.fundamental;
+
+import java.util.Objects;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String s) {
+        if (Objects.isNull(s)) return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("matao"));
+        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome(null));
+        System.out.println(isPalindrome(" m_m_m "));
+    }
+}
