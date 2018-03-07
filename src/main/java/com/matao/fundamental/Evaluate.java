6d7bd17e12ee25edf358efc51297ec6eef3c478f
matao <muxter.matao@gmail.com>
2018-03-07 22:27:02 +0800

    refactor(evaluate): extract evaluate method

--- src/main/java/com/matao/fundamental/Evaluate.java
+++ src/main/java/com/matao/fundamental/Evaluate.java
@@ -6,49 +6,63 @@ package com.matao.fundamental;
  * 1) 将操作数压入操作数栈
  * 2) 将运算符压入运算符栈
  * 3) 忽略左括号
- * 4) 遇到右括号时,弹出一个操作符,弹出所需数量的操作数,并将运算符和操作数的运算结果压入操作数栈
+ * 4) 遇到右括号时,弹出一个操作符,弹出所需数量的操作数,二元运算后弹出的操作数在前
+ * 并将运算符和操作数的运算结果压入操作数栈
  */
 public class Evaluate {
-    public static void main(String[] args) {
-        String exp = "(1+((2+3)*(4*5)))";     // expected 101
 
-        Stack<Character> operations = new Stack<>();
+    public static double evaluate(String exp) {
+        Stack<String> operators = new Stack<>();
         Stack<Double> values = new Stack<>();
-        for (int i = 0; i < exp.length(); i++) {
-            char c = exp.charAt(i);
-            switch (c) {
-                case '(':
+        for (String s : exp.split(" ")) {
+            switch (s) {
+                case "(":
                     break;
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                    operations.push(c);
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "sqrt":
+                    operators.push(s);
                     break;
-                case ')':
-                    char op = operations.pop();
-                    double val1 = values.pop();
-                    double val2 = values.pop();
-                    switch (op) {
-                        case '+':
-                            values.push(val2 + val1);
+                case ")":
+                    String operator = operators.pop();
+                    double value = values.pop();
+                    switch (operator) {
+                        case "+":
+                            values.push(values.pop() + value);
                             break;
-                        case '-':
-                            values.push(val2 - val1);
+                        case "-":
+                            values.push(values.pop() - value);
                             break;
-                        case '*':
-                            values.push(val2 * val1);
+                        case "*":
+                            values.push(values.pop() * value);
                             break;
-                        case '/':
-                            values.push(val2 / val1);
+                        case "/":
+                            values.push(values.pop() / value);
+                            break;
+                        case "sqrt":
+                            values.push(Math.sqrt(value));
+                            break;
+                        default:
                             break;
                     }
                     break;
-                default:
-                    values.push(Double.parseDouble(c + ""));
+                default:    // number
+                    values.push(Double.parseDouble(s));
                     break;
             }
         }
-        System.out.println(values.pop());
+        return values.pop();
+    }
+
+
+    public static void main(String[] args) {
+        String exp1 = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";     // expected 101.0
+        System.out.println(evaluate(exp1));
+
+        String exp2 = "( ( 1 + sqrt ( 5.0 ) ) / 2.0 )";     // expected 101
+        System.out.println(evaluate(exp2));
+
     }
 }
